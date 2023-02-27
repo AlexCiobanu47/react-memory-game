7464f3e66d00d4d4aa3765237d106aab39bb0bc4
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-02-27 10:03:42 +0200

    Added basic resposive design

--- src/Cards.js
+++ src/Cards.js
@@ -18,6 +18,12 @@ const Cards = ({cardsArray, handleCardClick}) =>{
 const CardsDiv = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    gap: 3rem
+    gap: 3rem;
+    @media (max-width: 1300px){
+        grid-template-columns: repeat(3, 1fr);
+    }
+    @media (max-width: 1100px){
+        grid-template-columns: repeat(2, 1fr);
+    }
 `
 export default Cards;
